6eed4e0074d3ab93771b3a437a76dee4d80eb021
Arthur Uzulin <fuj1nman@hotmail.com>
2013-07-22 23:01:49 +1000

    Tested dynamic collision boxes
    Tested the dynamic collision boxes, so far, I think using them is a
    terrible idea. (not using, finding another method)

--- modjam2_test/fuj1n/modjam2/test/ModJam2Test.java
+++ modjam2_test/fuj1n/modjam2/test/ModJam2Test.java
@@ -1,16 +1,22 @@
 package fuj1n.modjam2.test;
 
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.GameRegistry;
+import fuj1n.modjam2.test.block.BlockDynamicColBoxTest;
 
 @Mod(name="ModJam #2 Test - fuj1n", modid="fuj1n.modjam.v2.test", version="UNDEFINED")
 @NetworkMod(clientSideRequired=true, serverSideRequired=false)
 public class ModJam2Test {
 
+	public static Block dynColTest;
+	
 	@EventHandler
 	public void PreInit(FMLPreInitializationEvent event){
 		
@@ -18,7 +24,8 @@ public class ModJam2Test {
 	
 	@EventHandler
 	public void Init(FMLInitializationEvent event){
-		
+		dynColTest = new BlockDynamicColBoxTest(2300).setCreativeTab(CreativeTabs.tabBlock);
+		GameRegistry.registerBlock(dynColTest);
 	}
 	
 	@EventHandler
---
+++ modjam2_test/fuj1n/modjam2/test/block/BlockDynamicColBoxTest.java
@@ -0,0 +1,33 @@
+package fuj1n.modjam2.test.block;
+
+import java.util.List;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+public class BlockDynamicColBoxTest extends Block{
+
+	public BlockDynamicColBoxTest(int par1) {
+		super(par1, Material.craftedSnow);
+	}
+	
+	@Override
+	public void addCollisionBoxesToList(World par1World, int par2, int par3, int par4, AxisAlignedBB par5AxisAlignedBB, List par6List, Entity par7Entity) {
+		if(par1World.isBlockIndirectlyGettingPowered(par2, par3, par4)){
+			this.setBlockBounds(0F, 0F, 0F, 1F, 3F, 1F);
+		}else{
+			this.setBlockBounds(0F, 0F, 0F, 1F, 1F, 1F);
+		}
+		
+		super.addCollisionBoxesToList(par1World, par2, par3, par4, par5AxisAlignedBB, par6List, par7Entity);
+	}
+	
+	@Override
+    public void setBlockBoundsForItemRender() {
+		this.setBlockBounds(0F, 0F, 0F, 1F, 1F, 1F);
+	}
+}
